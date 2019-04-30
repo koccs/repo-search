78c7d624c5ed88f53dbd1740ca1ba6fcb54b6288
Ádám Kocsis <kocccs@gmail.com>
2019-04-30 20:34:09 +0200

    feat(details): route parameters is passed correctly
    route parameter getting and decoding is added and covered with tests

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from "@angular/router";
+import { ActivatedRouteStub } from "../../testing/activated-route-stub";
 
-import { DetailsComponent } from './details.component';
+import { DetailsComponent } from "./details.component";
 
-describe('DetailsComponent', () => {
+describe("DetailsComponent", () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
-
+  const activatedRoute = new ActivatedRouteStub();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [DetailsComponent],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    activatedRoute.setParamMap({ fullName: "test%2Frepo" });
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("propertyName should be set from route", () => {
+    expect(component.repositoryName).toBe("test/repo");
   });
 });
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.scss"]
 })
 export class DetailsComponent implements OnInit {
+  repositoryName: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const fullName = params.get("fullName");
+      this.repositoryName = decodeURIComponent(fullName);
+    });
   }
-
 }
