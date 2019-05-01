77220e9b7c205b69ad4a142aca8dadf0aa2ed0aa
Ádám Kocsis <kocccs@gmail.com>
2019-05-01 10:22:08 +0200

    test(resolver): details resolver test coverage is increased

--- src/app/services/details-resolver.service.spec.ts
+++ src/app/services/details-resolver.service.spec.ts
@@ -2,18 +2,44 @@ import { TestBed } from "@angular/core/testing";
 
 import { DetailsResolverService } from "./details-resolver.service";
 import { GithubService } from "./github.service";
+import { Repository } from "../../models/repository";
+import { Issue } from "../../models/issue";
+import { ActivatedRouteSnapshot } from "@angular/router";
+import { of } from "rxjs";
 
 describe("DetailsResolverService", () => {
-  let githubServiceSpy: { searchRepositories: jasmine.Spy };
+  let service: DetailsResolverService;
+  let githubServiceSpy: { getRepository: jasmine.Spy; getIssues: jasmine.Spy };
 
-  beforeEach(() =>
+  beforeEach(() => {
+    githubServiceSpy = jasmine.createSpyObj("GithubService", [
+      "getRepository",
+      "getIssues"
+    ]);
     TestBed.configureTestingModule({
       providers: [{ provide: GithubService, useValue: githubServiceSpy }]
-    })
-  );
+    });
+    service = TestBed.get(DetailsResolverService);
+
+    githubServiceSpy.getIssues.and.returnValue(of(new Array<Issue>()));
+    githubServiceSpy.getRepository.and.returnValue(of(new Repository("dummy")));
+  });
 
   it("should be created", () => {
-    const service: DetailsResolverService = TestBed.get(DetailsResolverService);
     expect(service).toBeTruthy();
+  });
+
+  describe("resolve calls", () => {
+    it("getRepository on github service", () => {
+      service.resolve(new ActivatedRouteSnapshot()).subscribe(() => {
+        expect(githubServiceSpy.getRepository.calls.count()).toBe(1);
+      });
+    });
+
+    it("getIssues on github service", () => {
+      service.resolve(new ActivatedRouteSnapshot()).subscribe(() => {
+        expect(githubServiceSpy.getIssues.calls.count()).toBe(1);
+      });
+    });
   });
 });
